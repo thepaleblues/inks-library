9b822c4861489fe8c7e7d13db723b714f996fb20
thepaleblues <292732688+thepaleblues@users.noreply.github.com>
2026-09-08 07:59:19 +0800

    fix(recommendations): improve book card responsiveness

--- src/ink/Recommendations.jsx
+++ src/ink/Recommendations.jsx
@@ -65,7 +65,7 @@ function RecoBooks({ booksToReco, setCurrentBook}) {
             grid 
             gap-4
             grid-cols-1 
-            sm:grid-cols-3
+            sm:grid-cols-2
             lg:grid-cols-3
          "
       >
